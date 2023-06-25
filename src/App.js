b66dee894bd1dc32072b27fa6e49234b0cd0f78b
Galvin <77013913+GalvinPython@users.noreply.github.com>
2023-06-25 21:37:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import logo from './logo.svg';
+import newLogo from './logoNew.svg'
 import './App.css';
+import Odometer from 'react-odometerjs'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState()
+  useEffect(() => {
+    let rows;
+    const interval = setInterval(() => {
+      console.log("Test")
+      fetch('https://api-gc.galvindev.me.uk/clicks', {method: 'GET'})
+      .then(function(response) {
+      if(response.ok) return response.json();
+      throw new Error('Request failed.');
+      })
+      .then(function(data) {
+        rows = data[0].row_count
+      })
+      .catch(function(error) {
+          console.log(error);
+      });
+
+      setValue(rows)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+  function sendReq() {
+    fetch('https://api-gc.galvindev.me.uk/clicked')
+    .then(function(response) {
+      if(response.ok) return response.json
+      throw new Error('Request failed.')
+    })
+    .catch(function(error) {
+      console.error(error)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={newLogo} className="App-logo" alt="Indicating a mouse cursor"/>
+        <p>Global Clicker By: <code>GalvinPython</code></p>
+        <div className='Odometer'>
+          <Odometer value={value} format='(,ddd),dd' style={{ fontWeight: "bold", whiteSpace: "nowrap", overflow: "hidden" }} title='Clicks / Count'/>
+        </div>
+        <button onClick={sendReq}>+1 Click</button>
+        <small title='This is a beta version to test everything works. Compiled on 21st June 2023'>Version 0.1-20230621 (BETA)</small>
       </header>
     </div>
   );

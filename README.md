f42d466e2554b423f5e7e241502e8ea3f1f65102
GalvinPython <77013913+GalvinPython@users.noreply.github.com>
2024-05-24 19:11:59 +0100

    brought the site back to life

--- README.md
+++ README.md
@@ -1,39 +1,26 @@
 # GlobalClicker
 [Click here to play](https://gc.galvindev.me.uk)  
-![Website](https://img.shields.io/website?url=https%3A%2F%2Fgc.galvindev.me.uk&style=for-the-badge&label=Website) ![API](https://img.shields.io/website?url=https%3A%2F%2Fapi-gc.galvindev.me.uk&style=for-the-badge&label=API)  
-![Website Version](https://img.shields.io/badge/Website%20Version%3A-1.0%20(Upcoming%201)-blue?style=for-the-badge)
+![Website](https://img.shields.io/website?url=https%3A%2F%2Fgc.galvindev.me.uk&style=for-the-badge&label=Website) ![API](https://img.shields.io/website?url=https%3A%2F%2Fapi-gc.galvindev.me.uk&style=for-the-badge&label=API)
 
 # Note
-The current contents of the upcoming branch are to be replaced with a new, more improved version. This branch exists to stop git complaining and why everything is a mess
+This clicker is no longer being actively maintained, and will only be getting security updates and such, as I have a new, improved clicker [here](https://github.com/stats100/clicker)
 
 # API Endpoints
 These are the currently working endpoints, however more are to be developed:
 * /clicks - Get the number of clicks **(GET)**
 
 # Changelog
-Note: Backend code isn't available, however changes are documented
-
-### Version 1.0 (Upcoming)
-#### Backend
-* No issues fixed, but there will be changes made
-
-#### Frontend
-* Button hover colour is now blue, rather than staying green. However it will remain green when you're not hovering
-* Added pointer when hovering on button
-* Can no longer select text
-* Renamed odometer.css
-* Made small text more readable on smaller devices
-* `index.html`: root is now main rather than div
-* Added accessability
-* Added light mode
-* Restructured folders
-
 <details>
+  
+  ### 1.0 (24/05/2024)
+  * Changed API response
+  * Out of beta because there's nothing else happening
+  
   <summary>Beta</summary>
   
   ### Beta 0.3.1 (18/09/2023)
   #### Frontend
-  * Fixed loading time "bug" (thanks Nia)
+  * Fixed loading time bug (thanks Nia)
 
   ### Beta 0.3 (26/07/2023)
   #### Frontend
--- src/App.js
+++ src/App.js
@@ -4,64 +4,64 @@ import Odometer from 'react-odometerjs'
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [value, setValue] = useState()
-  useEffect(() => {
-    let rows;
-    let loaded = false;
-    console.log("Script running!")
-    function getData() {
-      fetch('https://api-gc.galvindev.me.uk/clicks', { method: 'GET' })
-        .then(function (response) {
-          if (response.ok) return response.json();
-          throw new Error('Request failed.');
-        })
-        .then(function (data) {
-          rows = data[0].row_count
-          if (loaded === false) setValue(rows)
-          loaded = true;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+    const [value, setValue] = useState()
+    useEffect(() => {
+        let rows;
+        let loaded = false;
+        console.log("Script running!")
+        function getData() {
+            fetch('https://api-gc.galvindev.me.uk/clicks', { method: 'GET' })
+                .then(function (response) {
+                    if (response.ok) return response.json();
+                    throw new Error('Request failed.');
+                })
+                .then(function (data) {
+                    rows = data.clicks
+                    if (loaded === false) setValue(rows)
+                    loaded = true;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
 
-      setValue(rows)
+            setValue(rows)
+        }
+        getData();
+        const interval = setInterval(getData, 2000)
+        return () => clearInterval(interval)
+    }, [])
+
+    function sendReq() {
+        fetch('https://api-gc.galvindev.me.uk/clicked')
+            .then(function (response) {
+                if (response.ok) return response.json
+                throw new Error('Request failed.')
+            })
+            .catch(function (error) {
+                console.error(error)
+            })
     }
-    getData();
-    const interval = setInterval(getData, 2000)
-    return () => clearInterval(interval)
-  }, [])
 
-  function sendReq() {
-    fetch('https://api-gc.galvindev.me.uk/clicked')
-      .then(function (response) {
-        if (response.ok) return response.json
-        throw new Error('Request failed.')
-      })
-      .catch(function (error) {
-        console.error(error)
-      })
-  }
+    // Needed for outlinks
+    const Anchor = props => {
+        return (
+            <a href='https://github.com/galvinpython/globalclicker'>{props.children}</a>
+        )
+    }
 
-  // Needed for outlinks
-  const Anchor = props => {
     return (
-      <a href='https://github.com/galvinpython/globalclicker'>{props.children}</a>
-    )
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Global Clicker By: <code>GalvinPython</code></p>
-        <div className='Odometer'>
-          <Odometer value={value} format='(,ddd),dd' style={{ fontWeight: "bold", whiteSpace: "nowrap", overflow: "hidden" }} title='Clicks / Count' />
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Global Clicker By: <code>GalvinPython</code></p>
+                <div className='Odometer'>
+                    <Odometer value={value} format='(,ddd),dd' style={{ fontWeight: "bold", whiteSpace: "nowrap", overflow: "hidden" }} title='Clicks / Count' />
+                </div>
+                <button onClick={sendReq}>+1 Click</button>
+                <small><Anchor>View Source</Anchor></small>
+            </header>
         </div>
-        <button onClick={sendReq}>+1 Click</button>
-        <small title='This is a beta version to test everything works. Built on 18th September 2023'>Version 0.3.1-20230918 (BETA) | <Anchor>View Source</Anchor></small>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
